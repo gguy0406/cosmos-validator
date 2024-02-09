d351de67fc41e22d7a0b09895444d2c73c3ef317
Trung D. Ngo <gguy0406@gmail.com>
2024-02-09 05:19:20 +0000

    chore: fix

--- node/reset-state-sync.sh
+++ node/reset-state-sync.sh
@@ -4,7 +4,7 @@ $DAEMON_NAME tendermint unsafe-reset-all --keep-addr-book
 
 TRUST_HEIGHT=$(($(curl -s $RPC_ENDPOINT/block | jq -r .result.block.header.height) - "${1:-1000}"))
 TRUST_HASH=$(curl -s $RPC_ENDPOINT/block?height=$TRUST_HEIGHT | jq -r .result.block_id.hash)
-CONFIG_TOML=$NODE_HOME/config/confif.toml
+CONFIG_TOML=$NODE_HOME/config/config.toml
 
 sed -i "s/trust_height = .*/trust_height = \"$TRUST_HEIGHT\"/" $CONFIG_TOML
 sed -i "s/trust_hash = .*/trust_hash = \"$TRUST_HASH\"/" $CONFIG_TOML
